9d5eec7769df3aabe33429291d1b7fb27765ba6c
Alexander Proskuryakov <alexander.proskuryakov@outlook.com>
2019-08-26 13:40:32 -0400

    added beta

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BetaComponent } from './beta/beta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BetaComponent
   ],
   imports: [
     BrowserModule,
