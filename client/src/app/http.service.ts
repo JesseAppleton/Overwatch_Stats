6953e32c94d13a200a9095ee3c9470418008216c
Jesse Appleton <Appleton.Jesse@gmail.com>
2019-08-28 15:05:25 -0400

    More frontend

--- client/src/app/http.service.ts
+++ client/src/app/http.service.ts
@@ -10,6 +10,12 @@ export class HttpService {
 
   constructor(private _httpClient :  HttpClient) { }
 
+  player: any;
+
+  ngOnInit() {
+
+  }
+  
   getPlayers() {
     return this._httpClient.get("/players");
   }
@@ -17,14 +23,20 @@ export class HttpService {
   getOnePlayer(_id) {
     return this._httpClient.get(`/players/${_id}`);
   } 
-
-  updatePlayer(_id, player) {
-    return this._httpClient.put(`/players/${_id}`, player);
-  }
-
+  
   getPlayerStats(player) {
-    return this._httpClient.get(`http://overwatchy.com/profile/${player.platform}/${player.region}/${player.battleTag}`)
+    return this._httpClient.get(`http://overwatchy.com/profile/${player.platform}/${player.region}/${player.battleTag}`);
   }
+
+  onSubmit() {
+    this.getPlayers();
+    // this.getPlayerStats(this.player);
+    
+  }
+
+  // updatePlayer(_id, player) {
+  //   return this._httpClient.put(`/players/${_id}`, player);
+  // }
 
   // removePlayer(_id) {
   //   return this._httpClient.delete(`/players/${_id}`);
